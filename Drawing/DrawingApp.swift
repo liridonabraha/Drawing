be6e2365482294f3924345f8b247131da9117964
Dom Bryan <dombguitar@gmail.com>
2022-01-13 14:42:34 +0000

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Dom Bryan on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
